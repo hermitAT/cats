3919d140cd23faf9073107ccec03fadceb62db36
Adam Thorne <ahthornev2@gmail.com>
2020-11-04 04:06:51 +0000

    write both sync&async Mocha&Chai tests for async control control

---
+++ asyncBreeds.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = (breed, callback) => {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+
+    if (!error) {
+      callback(data);
+    } else {
+      callback(undefined);
+    }
+  });
+};
+
+const printOutBreed = breed => {
+  console.log('Return Value: ', breed);
+};
+
+module.exports = breedDetailsFromFile;
